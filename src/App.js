aae83e889a7a8ad00a8812eabb4a536139f16ebc
dhruv1145 <65786137+dhruv1145@users.noreply.github.com>
2024-12-07 21:24:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
+import Achievement from "./components/Resume/Achievement";
 import {
   BrowserRouter as Router,
   Route,
@@ -16,6 +17,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ResumeNew from "./components/Resume/ResumeNew";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -38,7 +40,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/achievement" element={<Achievement />} />
+          <Route path="/resume" element={<ResumeNew />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
